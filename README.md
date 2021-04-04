f12dfb5a49ff1a2c88b2ac65e4ee2e3da194e959
Rustam-Zokirov <rz.zokirov@gmail.com>
2021-04-04 16:23:47 +0500

    updated

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# Lab 6
+# Lab Assignment 6
 
 [![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=4431213&assignment_repo_type=AssignmentRepo)
 
 - Student: Rustam Zokirov
 - ID: U1910049
 - Section: 002
+
+## Extra
+- Video lecture - https://www.youtube.com/watch?v=bvQZ7HkCKNg
+- https://t.me/iut_ip2021/22
+
+## Checkpoints
+- <img src="img/1.png" width=600">
 
 # Extra
 - Video lecture - https://www.youtube.com/watch?v=bvQZ7HkCKNg
--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -2,8 +2,9 @@
 
 	$pattern="";
 	$text="";
-	$replaceText="";
-	$replacedText="";
+	$replaceText=""; # replace the pattern
+	$replacedText=""; # replaced with pattern
+
 
 	$match="Not checked yet.";
 
@@ -15,28 +16,37 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	$replacedText=preg_replace($pattern, $replaceText, $text);
 
 	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+            $match="Match!";
+	} else {
+	    $match="Does not match!";
+	}
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Valid Form</title>
+	<title>Validate Form</title>
 </head>
 <body>
 	<form action="regex_valid_form.php" method="post">
 		<dl>
-			<dt>Pattern</dt>
-			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
+            <dt>Choose a pattern:</dt>
+            <dd>
+                <select name="pattern" id="pattern">
+                    <option value="/quick/">Check for word 'quick'</option>
+                    <option value="/[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}/">Validate email address</option>
+                    <option value="/\+998-\d{2}-\d{3}-\d{2}-\d{2}/">Validate phone number</option>
+                    <option value="/\s/">Write without spaces</option>
+                    <option value="/[^0-9\.,]/">Remove non-numeric</option>
+                    <option value="/\n/">Remove new lines</option>
+                    <option value="/\[(.*?)\]/">Extract text from parenthesis</option>
+                </select>
+            </dd>
 
-			<dt>Text</dt>
+            <dt>Text</dt>
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
 
 			<dt>Replace Text</dt>
@@ -46,7 +56,8 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 			<dd><?=	$match ?></dd>
 
 			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
+			<dd> <code><?= $replacedText ?></code></dd>
+
 
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
